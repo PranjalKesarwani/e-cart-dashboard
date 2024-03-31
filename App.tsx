d4fa6eba80008dd2dae118c89322f806ffa4f4d1
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2024-03-31 15:36:05 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+      <>
+      <View>
+        <Text>
+          Hello World! Pranjal is here, i am going to make zomato!
+          I am planning to build a small company through which i 
+        </Text>
+      </View>
+      </>
+  );
+}
+
+
+
+export default App;
